59c33e03913f7a1972dedbdf223330765f4f26c4
medonne <medonnepenteado@gmail.com>
2026-09-15 15:57:30 -0300

    Adiciona validação para continuar o jogo

--- Desafio1/desafio01.py
+++ Desafio1/desafio01.py
@@ -1,41 +1,42 @@
 # Desafio de Projeto 01 - Lógica de Programação
 # Por: Médonne Penteado
 
-continuar = "sim"
+continuar = 'sim'
 
 # Laço de repetição
-while continuar == "sim":
+while continuar == 'sim':
 
     # Variáveis
-    heroi = input("Digite o nome do herói: ")
-    xp = int(input("Digite a quantidade de XP: "))
+    heroi = input('Digite o nome do herói: ')
+    xp = int(input('Digite a quantidade de XP: '))
 
     # Estrutura de decisão
     if xp <= 1000:
-        nivel = "Ferro"
+        nivel = 'Ferro'
     elif xp <= 2000:
-        nivel = "Bronze"
-
+        nivel = 'Bronze'
     elif xp <= 5000:
-        nivel = "Prata"
-
+        nivel = 'Prata'
     elif xp <= 7000:
-        nivel = "Ouro"
-
+        nivel = 'Ouro'
     elif xp <= 8000:
-        nivel = "Platina"
-
+        nivel = 'Platina'
     elif xp <= 9000:
-        nivel = "Ascendente"
-
+        nivel = 'Ascendente'
     elif xp <= 10000:
-        nivel = "Imortal"
-
+        nivel = 'Imortal'
     else:
-        nivel = "Radiante"
+        nivel = 'Radiante'
 
     # Saída
-    print(f"O Herói de nome {heroi} está no nível de {nivel}")
+    print(f'O herói de nome {heroi} está no nível de {nivel}')
 
     # Pergunta se deseja repetir
-    continuar = input("Deseja cadastrar outro herói? sim/não: ")
+    continuar = input('Deseja cadastrar outro herói? sim/não: ').lower()
+
+while continuar != 'sim' and continuar != 'não' and continuar != 'nao':
+    print('Opção inválida!')
+    continuar = input('Digite apenas sim ou não: ').lower()
+
+if continuar == 'não' or continuar == 'nao':
+    print('Obrigado por jogar!')
